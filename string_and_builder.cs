e3b9b65a25be916e4a28aee101b7cfb482ac6fe5
Sumit Rana <ranasumit258@gmail.com>
2022-08-03 14:45:12 +0530

    string concatenate and builder
    string concatenate and builder

--- string_and_builder.cs
+++ string_and_builder.cs
@@ -13,19 +13,22 @@ namespace string_and_builder
             string str1 = "Sumit";
             string str2 = "Dinesh";
             string str3 = "Rana";
-            string_and_builder.build.builder(str1,str2,str3);
-            string_and_builder.string_concatenate.concatenate(str1,str3);
+            string str4 = " ";
+            string_and_builder.build.builder(str1,str2,str3,str4);
+            string_and_builder.string_concatenate.concatenate(str1,str3,str4);
             
          }
     }
     class build
     {
-        public static void builder(string str1,string str2,string str3)
+        public static void builder(string str1,string str2,string str3,string str4)
 
         {
             StringBuilder addWord = new StringBuilder();
             addWord.Append(str1);
+            addWord.Append(str4);
             addWord.Append(str2);
+            addWord.Append(str4);
             addWord.Append(str3);
             Console.WriteLine(addWord);
          }
@@ -33,10 +36,10 @@ namespace string_and_builder
     }
     class string_concatenate
     {
-        public static void concatenate(string str1, string str3)
+        public static void concatenate(string str1, string str3,string str4)
         {
             string resultCon;
-            resultCon = str1+str3;
+            resultCon = str1+str4+str3;
             Console.WriteLine("The string concetanation is: " + resultCon);
 
         }
